9d41f72bf4238c2183f19f0a9654ffca3fbdb485
Julien FAUCHER <suzizecat@amidemoi.com>
2012-08-20 16:59:52 +0200

    Ajout de la fonction bool ouiNon()

---
+++ gestionAffichage.cpp
@@ -0,0 +1,61 @@
+#include "gestionAffichage.hpp"
+
+void info(QWidget *parent, QString contenu)
+{
+    QMessageBox::information(parent,"AlgoCreator v"+QCoreApplication::applicationVersion(),contenu);
+}
+void info(QWidget *parent,QString titre ,QString contenu)
+{
+    QMessageBox::information(parent,titre,contenu);
+}
+
+void avertissement(QWidget *parent, QString contenu)
+{
+    QMessageBox::warning(parent,"AlgoCreator v"+QCoreApplication::applicationVersion(),contenu);
+}
+void avertissement(QWidget *parent,QString titre ,QString contenu)
+{
+    QMessageBox::warning(parent,titre,contenu);
+}
+
+void alerte(QWidget *parent, QString contenu)
+{
+    QMessageBox::critical(parent,"AlgoCreator v"+QCoreApplication::applicationVersion(),contenu);
+}
+void alerte(QWidget *parent,QString titre ,QString contenu)
+{
+    QMessageBox::critical(parent,titre,contenu);
+}
+
+bool ouiNon(QWidget *parent, QString contenu)
+{
+   int i = QMessageBox::question(parent,"AlgoCreator v"+QCoreApplication::applicationVersion(),contenu,QMessageBox::Yes|QMessageBox::No);
+
+   if (i==QMessageBox::Yes)
+       return true;
+   else
+       return false;
+
+}
+bool ouiNon(QWidget *parent,QString titre ,QString contenu)
+{
+   int i = QMessageBox::question(parent,titre,contenu,QMessageBox::Yes|QMessageBox::No);
+
+   if (i==QMessageBox::Yes)
+       return true;
+   else
+       return false;
+}
+
+QString intVersStr(int nombre)
+{
+    return QVariant(nombre).toString();
+}
+
+void changeTitreFen(QWidget *parent, QString titre,bool titreComplet)
+{
+    if(titreComplet)
+        parent->setWindowTitle(titre);
+    else
+        parent->setWindowTitle(QString("AlgoCreator v"+QCoreApplication::applicationVersion()+" "+titre));
+}
--- gestionAffichage.hpp
+++ gestionAffichage.hpp
@@ -15,5 +15,11 @@ void avertissement(QWidget *parent,QString titre ,QString contenu);
 void alerte(QWidget *parent, QString contenu);
 void alerte(QWidget *parent,QString titre ,QString contenu);
 
+
+bool ouiNon(QWidget *parent, QString contenu);
+bool ouiNon(QWidget *parent,QString titre ,QString contenu);
+
+void changeTitreFen(QWidget *parent, QString titre,bool titreComplet=false);
+
 QString intVersStr(int nombre);
 #endif // GESTIONAFFICHAGE_HPP
